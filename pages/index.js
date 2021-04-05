2d3e69d1e3da4edd0cbab22068d4844e1f44f6f4
VascoManuel <81938804+VascoManuel@users.noreply.github.com>
2021-04-05 01:14:10 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -25,4 +25,24 @@ function Contador() {
         
 }
 
+<div class="container mt-2">
+<div class="row">
+  <div class="col-12">
+    <div class="jumbotron">
+      <h1 class="text-center">O jeito mais fácil de fazer suas impressões e fotos!</h1>
+      <div class="row justify-content-center">
+        <div class="col-auto">
+          <p><a class="btn btn-success btn-lg" href="#" role="button">IMPRESSÕES</a>&nbsp;</p>
+        </div>
+        <div class="col-auto">
+          <p><a class="btn btn-primary btn-lg" href="#" role="button">FOTOS</a>&nbsp;</p>
+        </div>
+      </div>
+      <p class="text-center">Você pode nos achar em um grande shopping center de São Paulo, em horários estendidos e inclusive finais de semana e feriados!</p>
+      <img src="images/1200x400_Reabertura.jpg" alt="" class="img-fluid"> 
+    </div>
+  </div>
+</div>
+</div>
+
 export default Home
