9d2f778ecb00367f5d73b9f9cd21395de89c71e9
VascoManuel <81938804+VascoManuel@users.noreply.github.com>
2021-04-05 01:22:09 -0300

    new home

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ function Contador() {
         </div>
       </div>
       <p class="text-center">Você pode nos achar em um grande shopping center de São Paulo, em horários estendidos e inclusive finais de semana e feriados!</p>
-      <img src="images/1200x400_Reabertura.jpg" alt="" class="img-fluid"> 
+      <img src="images/1200x400_Reabertura.gif" alt="" class="img-fluid"> 
     </div>
   </div>
 </div>
